3a610304cc0f6d8fa57f635519366f674e91e9ba
IgnacioSallaberry <42651786+IgnacioSallaberry@users.noreply.github.com>
2019-04-10 12:43:06 -0300

    La función grafica bien en 2dimensiones para un D dado, por ejemplo para D=90

--- como_varia_G_variando_D.py
+++ como_varia_G_variando_D.py
@@ -7,29 +7,37 @@ Created on Tue Apr  9 20:25:06 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
+import array
+
 
 box_size = 256
 roi = 64
 tp = 5e-6    #seg
-tl = roi * tp    #seg
-dr = 0.05   # delta r = pixel size =  nanometros
-w0 = 0.25   #radio de la PSF
-wz = 1.5   #alto de la PSF desde el centro
+tl = box_size * tp    #seg
+dr = 0.05   # delta r = pixel size =  micrometros
+w0 = 0.25   #radio de la PSF  = micrometros
+wz = 1.5   #alto de la PSF desde el centro  = micrometros
 gamma = 0.3536   #gamma factor de la 3DG
-N = 200   #Numero total de particulas
+N = 0.0176   #Numero total de particulas en la PSF
 
 
-D=90
-x0 = (box_size - roi)/2
-xf = (box_size + roi)/2
-x = np.linspace(x0,xf,(xf-x0)/dr) 
+D=90   #micrones^2/seg
+x0 = (box_size)/2  #seda inicial
+xf = (box_size + roi)/2  #seda final
+x=np.arange(0,xf-x0)
+
 y = 0  #miro solo la componente H orizontal
 
-####                                #termino de scanning                                             #termino de correlación espacial
-Gtot = np.exp(-0.5*((2*x*dr/w0)**2+(2*y*dr/w0)**2)/(1 + 4*D*(tp*x+tl*y)/(w0**2))) * (gamma/N)*( 1 + 4*D*(tp*x+tl*y) / w0**2 )**(-1) * ( 1 + 4*D*(tp*x+tl*y) / wz**2 )**(-1/2)
+###termino de scanning                                            
+S = np.exp(-0.5*((2*x*dr/w0)**2+(2*y*dr/w0)**2)/(1 + 4*D*(tp*x+tl*y)/(w0**2)))
+###termino de correlación espacial
+G = (gamma/N)*( 1 + 4*D*(tp*x+tl*y) / w0**2 )**(-1) * ( 1 + 4*D*(tp*x+tl*y) / wz**2 )**(-1/2)
 
+
+Gtotal = S * G
+Gtotal_normalizada = Gtotal/max(Gtotal)
 ### Plot the surface.
-plt.plot(x,Gtot)
+plt.plot(np.log10(x),Gtotal_normalizada,'-*')
 plt.show()
 
 
