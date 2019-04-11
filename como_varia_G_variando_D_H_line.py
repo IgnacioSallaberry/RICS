b2087347752073db27f89e7de467726d55b47e82
IgnacioSallaberry <42651786+IgnacioSallaberry@users.noreply.github.com>
2019-04-11 17:57:33 -0300

    Agregué modificaciones en los graficos
    Ahora puedo graficar H y V line todo de un saque.
    para graficar V line redefini el tp = tl y agregué de nuevo las funciones S, Gdiff y Gtot
    Entonces el programa:
    Grafica las tres curvas en H line para un D
    Grafica las 3 curvas en V line para el mismo D
    luego pasa al D siguiente y repite

--- como_varia_G_variando_D_H_line.py
+++ como_varia_G_variando_D_H_line.py
@@ -10,11 +10,11 @@ import matplotlib.pyplot as plt
 
 
 
-box_size = 1024
-roi = 1024
+box_size = 256
+roi = 64
 tp = 5e-6    #seg    
 tl = box_size * tp    #seg
-dr = 0.025   # delta r = pixel size =  micrometros
+dr = 0.05   # delta r = pixel size =  micrometros
 w0 = 0.25   #radio de la PSF  = micrometros    
 wz = 1.5   #alto de la PSF desde el centro  = micrometros
 gamma = 0.3536   #gamma factor de la 3DG
@@ -22,8 +22,8 @@ N = 0.0176   #Numero total de particulas en la PSF
 
 
 
-
-D=np.arange(1,1050,200)   #micrones^2/seg
+D=[1,10,90,100]
+#D=np.arange(1,1050,200)   #micrones^2/seg
 x0 = (box_size)/2  #seda inicial
 xf = (box_size + roi)/2  #seda final
 x=np.arange(1,xf-x0)
@@ -42,6 +42,10 @@ fig = plt.figure()
     
 
 for d in D:
+    
+    tp = 5e-6    #seg    
+    tl = box_size * tp    #seg
+    
     S = np.exp(-0.5*((2*x*dr/w0)**2+(2*y*dr/w0)**2)/(1 + 4*d*(tp*x+tl*y)/(w0**2)))
 ###termino de correlación espacial
     G = (gamma/N)*( 1 + 4*d*(tp*x+tl*y) / w0**2 )**(-1) * ( 1 + 4*d*(tp*x+tl*y) / wz**2 )**(-1/2)
@@ -57,9 +61,9 @@ for d in D:
     plt.figure(1)
     plt.semilogx((dr*x), G_norm,'-.',label=f'D={d}')
     plt.legend()
-    plt.xlabel(r'pixel shift $\psi$ - $\mu m$',fontsize=14)
-    plt.ylabel(r'Gdiff($\psi$)',fontsize=14)
-    plt.title('H-line G diff  \n tl = {}$ms$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e3,dr,box_size),fontsize=18)
+    plt.xlabel(r'pixel shift $\xi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Gdiff($\xi$)',fontsize=14)
+    plt.title('H-line G diff  \n tp = {}$\mu$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e6,dr,box_size),fontsize=18)
     plt.show()
     
     
@@ -67,29 +71,66 @@ for d in D:
     plt.figure(2)
     plt.semilogx((dr*x), S_norm,'-.',label=f'D={d}')
     plt.legend()
-    plt.xlabel(r'pixel shift $\psi$ - $\mu m$',fontsize=14)
-    plt.ylabel(r'Scanning($\psi$)',fontsize=14)
-    plt.title('H-line  SCANNING  \n tl = {}$ms$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e3,dr,box_size),fontsize=18)
+    plt.xlabel(r'pixel shift $\xi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Scanning($\xi$)',fontsize=14)
+    plt.title('H-line  SCANNING  \n tp = {}$\mu$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e6,dr,box_size),fontsize=18)
     plt.show()
 
     ###  Grafico funcion de correlación  total
     plt.figure(3)
     plt.semilogx((dr*x), Gtotal_normalizada,'-.',label=f'D={d}')
     plt.legend()
-    plt.xlabel(r'pixel shift $\psi$ - $\mu m$',fontsize=14)
-    plt.ylabel(r'Gtot($\psi$)',fontsize=14)
-    plt.title('H-line G total \n tl = {}$ms$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e3,dr,box_size),fontsize=18)
-
+    plt.xlabel(r'pixel shift $\xi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Gtot($\xi$)',fontsize=14)
+    plt.title('H-line G total \n tp = {}$\mu$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e6,dr,box_size),fontsize=18)
+    plt.show()
     
 #    i=0
 #    while Gtotal_normalizada[i]>0.60:
 #        i+=1
 #    plt.semilogx(dr*x[i],Gtotal_normalizada[i], '*', label=f'dist para Gtotal/2 = {round(dr*x[i],5)}$\mu m$')      ###---> para graficar punto donde cae a la mitad la curva de correlación
+
+    
+### -- #si quiero mirar la V line  hago tp=tl, esto puedo hacerlo que poner "x=0" y activar "y=np.arange(1,xf-x0)" es lo mismo que dejar x=0 y poner tp=tl
+    tp=tl
+
+    S = np.exp(-0.5*((2*x*dr/w0)**2+(2*y*dr/w0)**2)/(1 + 4*d*(tp*x+tl*y)/(w0**2)))
+###termino de correlación espacial
+    G = (gamma/N)*( 1 + 4*d*(tp*x+tl*y) / w0**2 )**(-1) * ( 1 + 4*d*(tp*x+tl*y) / wz**2 )**(-1/2)
+
+
+    Gtotal = S * G
+    Gtotal_normalizada = Gtotal/max(Gtotal)
+    S_norm = S/max(S)
+    G_norm = G/max(G)
+    
+    ###  Grafico solo termino difussivo
+    plt.figure(4)
+    plt.semilogx((dr*x), G_norm,'-.',label=f'D={d}')
+    plt.legend()
+    plt.xlabel(r'pixel shift $\psi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Gdiff($\psi$)',fontsize=14)
+    plt.title('V-line G diff  \n tl = {}$ms$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e3,dr,box_size),fontsize=18)
+    plt.show()
+    
+    
+    ###  Grafico solo termino Scanning
+    plt.figure(5)
+    plt.semilogx((dr*x), S_norm,'-.',label=f'D={d}')
+    plt.legend()
+    plt.xlabel(r'pixel shift $\psi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Scanning($\psi$)',fontsize=14)
+    plt.title('V-line  SCANNING  \n tl = {}$ms$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e3,dr,box_size),fontsize=18)
     plt.show()
 
-
-
-
+    ###  Grafico funcion de correlación  total
+    plt.figure(6)
+    plt.semilogx((dr*x), Gtotal_normalizada,'-.',label=f'D={d}')
+    plt.legend()
+    plt.xlabel(r'pixel shift $\psi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Gtot($\psi$)',fontsize=14)
+    plt.title('V-line G total \n tl = {}$ms$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e3,dr,box_size),fontsize=18)
+    plt.show()
 
 
 
