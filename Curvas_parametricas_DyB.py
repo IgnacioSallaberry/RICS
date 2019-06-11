671f24d889c626a52f2f04405633848b21cb6b7a
IgnacioSallaberry <42651786+IgnacioSallaberry@users.noreply.github.com>
2019-06-11 11:04:06 -0300

    Actualizacion
    Ahora grafica todas las curvas: factores de la funcion de correlación y la ACF total.
    Además se puede poner un punto con una flecha que marca la coordenada en x para la cual el valor de la ACF total cae a la mitad.

--- Curvas_parametricas_DyB.py
+++ Curvas_parametricas_DyB.py
@@ -23,25 +23,27 @@ wz = 1.5              #alto de la PSF desde el centro  = micrometros
 gamma = 0.3536        #gamma factor de la 3DG
 N = 0.0176            #Numero total de particulas en la PSF
 
-At= 1                 #Amplitud de triplete
+At= 0                 #Amplitud de triplete
 t_triplet = .16       #segundos    #Tiempo caracteristico de triplete 
 
-Ab= 1                 #Amplitud de binding
-t_binding= .16        #segundos     #Tiempo caracteristico de binding
+Ab= 0.93                 #Amplitud de binding
+t_binding= .016        #segundos     #Tiempo caracteristico de binding
 
 
 
-D=[0.1,1,10,100,1000]
+D=[10]
+#D=[0.1,1,10,100,1000]
 #D=np.arange(1,1050,200)   #micrones^2/seg
+
 x0 = (box_size)/2  #seda inicial
 xf = (box_size + roi)/2  #seda final
 #xf=640
 x=np.arange(1,xf-x0)
-
 y = 0  #miro solo la componente H orizontal
 
-
-###------------------------------------------------GRAFICO EN 2D
+#==============================================================================
+#                                Tipografía de los gráficos
+#==============================================================================    
 SMALL_SIZE = 14
 MEDIUM_SIZE = 16
 BIGGER_SIZE = 18
@@ -55,6 +57,11 @@ plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 
+#==============================================================================
+#                                  
+#                           GRAFICOS EN 2D
+#
+#==============================================================================    
 fig = plt.figure()    
 plt.close('all') # amtes de graficar, cierro todos las figuras que estén abiertas
     
@@ -65,46 +72,34 @@ for d in D:
 #    else:
 #        guardar_imagenes = False
 #    
+    i=1   #con este indice voy a numerar las imagenes. Lo pongo dado que si quiero modificar el codigo tengo que modificar el plt.figure(3) por ejemplo.En cambio con esto es automático
+#
 #==============================================================================
 #                                  H - Line
 #==============================================================================    
 
-###termino de scanning
+    ###termino de scanning
     S = np.exp(-0.5*((2*x*dr/w0)**2+(2*y*dr/w0)**2)/(1 + 4*d*(tp*x+tl*y)/(w0**2)))
-###termino de correlación espacial
+    ###termino de correlación espacial
     G_diff = (gamma/N)*( 1 + 4*d*(tp*x+tl*y) / w0**2 )**(-1) * ( 1 + 4*d*(tp*x+tl*y) / wz**2 )**(-1/2)
-###termino de triplete
+    ###termino de triplete
     G_triplet = 1+ At * np.exp(-(tp*x+tl*y)/t_triplet)
-###termino de binding
+    ###termino de binding
     G_bind =Ab * np.exp(-(x*dr/w0)**2-(y*dr/w0))* np.exp(-(tp*x+tl*y)/t_binding)
 
-###Total ACF    
+    ###Total ACF    
     Gtotal = S * G_diff * G_triplet + G_bind
     Gtotal_normalizada = Gtotal/max(Gtotal)
 
-###Normalización de factores de la ACF    
+    ###Normalización de factores de la ACF    
     S_norm = S/max(S)
     G_diff_norm = G_diff/max(G_diff)
     G_triplet_norm = G_triplet/max(G_triplet)
     G_bind_norm = G_bind/max(G_bind)    
     
-    ###  Grafico solo termino difussivo
-    plt.figure(1)
-#    plt.plot((dr*x), G_norm,'-.',label=f'D={d}',linewidth=1)
-    plt.semilogx((dr*x), G_diff_norm,'-.',label=f'D={d}')
-    plt.legend()
-    plt.xlabel(r'pixel shift $\xi$ - $\mu m$',fontsize=14)
-    plt.ylabel(r'Gdiff($\xi$)',fontsize=14)
-#    plt.title('H-line G diff  \n tp = {}$\mu$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e6,dr,box_size),fontsize=18)
-    plt.title('H-line G diff')
-    plt.show()
-    plt.tight_layout() #hace que no me corte los márgenes
     
-    if guardar_imagenes:
-        plt.savefig('C:\\Users\\LEC\\Desktop\\Poster TOPFOT 2019\\H_Line_Diff')
-    
-    ###  Grafico solo termino Scanning
-    plt.figure(2)
+     ###  Grafico solo termino Scanning
+    plt.figure(i)
 #    plt.plot((dr*x), S_norm,'-.',label=f'D={d}',linewidth=1)
     plt.semilogx((dr*x), S_norm,'-.',label=f'D={d}')
     plt.legend()
@@ -116,10 +111,63 @@ for d in D:
     plt.tight_layout() #hace que no me corte los márgenes
     
     if guardar_imagenes:
-        plt.savefig('C:\\Users\\LEC\\Desktop\\Poster TOPFOT 2019\\H_Line_Scanning')
-        
+        plt.savefig('C:\\Users\\LEC\\Desktop')
+    i+=1   
+    
+    
+    ###  Grafico solo termino difussivo
+    plt.figure(i)
+#    plt.plot((dr*x), G_norm,'-.',label=f'D={d}',linewidth=1)
+    plt.semilogx((dr*x), G_diff_norm,'-.',label=f'D={d}')
+    plt.legend()
+    plt.xlabel(r'pixel shift $\xi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Gdiff($\xi$)',fontsize=14)
+#    plt.title('H-line G diff  \n tp = {}$\mu$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e6,dr,box_size),fontsize=18)
+    plt.title('H-line G diff')
+    plt.show()
+    plt.tight_layout() #hace que no me corte los márgenes
+    
+    if guardar_imagenes:
+        plt.savefig('C:\\Users\\LEC\\Desktop')
+    i+=1
+
+
+    ###  Grafico solo termino triplete
+    plt.figure(i)
+#    plt.plot((dr*x), G_norm,'-.',label=f'D={d}',linewidth=1)
+    plt.semilogx((dr*x), G_triplet_norm,'-.',label=f'D={d}')
+    plt.legend()
+    plt.xlabel(r'pixel shift $\xi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Gtriplet($\xi$)',fontsize=14)
+#    plt.title('H-line G diff  \n tp = {}$\mu$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e6,dr,box_size),fontsize=18)
+    plt.title('H-line G triplete')
+    plt.show()
+    plt.tight_layout() #hace que no me corte los márgenes
+    
+    if guardar_imagenes:
+        plt.savefig('C:\\Users\\LEC\\Desktop')
+    i+=1
+    
+    
+    ###  Grafico solo termino binding
+    plt.figure(i)
+#    plt.plot((dr*x), G_norm,'-.',label=f'D={d}',linewidth=1)
+    plt.semilogx((dr*x), G_bind_norm,'-.',label=f'D={d}')
+    plt.legend()
+    plt.xlabel(r'pixel shift $\xi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Gbind($\xi$)',fontsize=14)
+#    plt.title('H-line G diff  \n tp = {}$\mu$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e6,dr,box_size),fontsize=18)
+    plt.title('H-line G binding')
+    plt.show()
+    plt.tight_layout() #hace que no me corte los márgenes
+    
+    if guardar_imagenes:
+        plt.savefig('C:\\Users\\LEC\\Desktop')
+    i+=1
+    
+    
     ###  Grafico funcion de correlación  total
-    plt.figure(3)
+    plt.figure(i)
 #    plt.plot((dr*x), Gtotal_normalizada,'-.',label=f'D={d}',linewidth=1)
     plt.semilogx((dr*x), Gtotal_normalizada,'-.',label=f'D={d}')
     plt.legend()
@@ -131,81 +179,129 @@ for d in D:
     plt.tight_layout() #hace que no me corte los márgenes
     
     if guardar_imagenes:
-        plt.savefig('C:\\Users\\LEC\\Desktop\\Poster TOPFOT 2019\\H_Line_Gtot_norm')
+        plt.savefig('C:\\Users\\LEC\\Desktop')
+    i+=1
     
-#    i=0
-#    while Gtotal_normalizada[i]>0.60:
-#        i+=1
-#    plt.semilogx(dr*x[i],Gtotal_normalizada[i], '*', label=f'dist para Gtotal/2 = {round(dr*x[i],5)}$\mu m$')      ###---> para graficar punto donde cae a la mitad la curva de correlación
+    
+#    j=0
+#    while Gtotal_normalizada[j]>0.60:
+#        j+=1
+#    plt.semilogx(dr*x[j],Gtotal_normalizada[j], 'r*', label=f'dist para Gtotal/2 = {round(dr*x[j],5)}$\mu m$')      ###---> para graficar punto donde cae a la mitad la curva de correlación
+###    plt.semilogx(dr*x[j],0, 'g*', label=f'dist para Gtotal/2 = {round(dr*x[j],5)}$\mu m$')      ###---> para graficar punto en x donde cae a la mitad la curva de correlación
+#    plt.text(dr*x[j]+0.1, Gtotal_normalizada[j], r'$\xi$ = {} $\mu m$'.format(round(dr*x[j],3)), bbox=dict(facecolor='none', edgecolor='black', boxstyle='round,pad=1'))
+#    plt.arrow(dr*x[j], Gtotal_normalizada[j], 0, -Gtotal_normalizada[j])
 
-    
+
 #==============================================================================
 #                                  V - Line
 #==============================================================================    
 
 ####  si quiero mirar la V line  hago tp=tl, esto puedo hacerlo que poner "x=0" y activar "y=np.arange(1,xf-x0)" es lo mismo que dejar x=0 y poner tp=tl
-    
     tp=tl
-###termino de scanning
+
+ ###termino de scanning
     S = np.exp(-0.5*((2*x*dr/w0)**2+(2*y*dr/w0)**2)/(1 + 4*d*(tp*x+tl*y)/(w0**2)))
-###termino de correlación espacial
+    ###termino de correlación espacial
     G_diff = (gamma/N)*( 1 + 4*d*(tp*x+tl*y) / w0**2 )**(-1) * ( 1 + 4*d*(tp*x+tl*y) / wz**2 )**(-1/2)
+    ###termino de triplete
+    G_triplet = 1+ At * np.exp(-(tp*x+tl*y)/t_triplet)
+    ###termino de binding
+    G_bind =Ab * np.exp(-(x*dr/w0)**2-(y*dr/w0))* np.exp(-(tp*x+tl*y)/t_binding)
 
-
-    Gtotal = S * G_diff
+    ###Total ACF    
+    Gtotal = S * G_diff * G_triplet + G_bind
     Gtotal_normalizada = Gtotal/max(Gtotal)
+
+    ###Normalización de factores de la ACF    
     S_norm = S/max(S)
     G_diff_norm = G_diff/max(G_diff)
-
-    
-    ###  Grafico solo termino difussivo
-    plt.figure(4)
-#    plt.plot((dr*x), G_norm,'-.',label=f'D={d}')
-    plt.semilogx((dr*x), G_diff_norm,'-.',label=f'D={d}')
-    plt.legend()
-    plt.xlabel(r'pixel shift $\psi$ - $\mu m$',fontsize=14)
-    plt.ylabel(r'Gdiff($\psi$)',fontsize=14)
-#    plt.title('V-line G diff  \n tl = {}$ms$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e3,dr,box_size),fontsize=18)
-    plt.title('V-line G diff',fontsize=18)
-    plt.show()
-    plt.tight_layout() #hace que no me corte los márgenes
-    
-    if guardar_imagenes:
-        plt.savefig('C:\\Users\\LEC\\Desktop\\Poster TOPFOT 2019\\V_Line_Diff')
+    G_triplet_norm = G_triplet/max(G_triplet)
+    G_bind_norm = G_bind/max(G_bind)    
     
     
-    ###  Grafico solo termino Scanning
-    plt.figure(5)
-#    plt.plot((dr*x), S_norm,'-.',label=f'D={d}')
+     ###  Grafico solo termino Scanning
+    plt.figure(i)
+#    plt.plot((dr*x), S_norm,'-.',label=f'D={d}',linewidth=1)
     plt.semilogx((dr*x), S_norm,'-.',label=f'D={d}')
     plt.legend()
-    plt.xlabel(r'pixel shift $\psi$ - $\mu m$',fontsize=14)
-    plt.ylabel(r'Scanning($\psi$)',fontsize=14)
-#    plt.title('V-line  SCANNING  \n tl = {}$ms$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e3,dr,box_size),fontsize=18)
+    plt.xlabel(r'pixel shift $\xi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Scanning($\xi$)',fontsize=14)
+#    plt.title('H-line  SCANNING  \n tp = {}$\mu$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e6,dr,box_size),fontsize=18)
     plt.title('V-line  SCANNING')
     plt.show()
     plt.tight_layout() #hace que no me corte los márgenes
     
     if guardar_imagenes:
-        plt.savefig('C:\\Users\\LEC\\Desktop\\Poster TOPFOT 2019\\V_Line_Scanning')
+        plt.savefig('C:\\Users\\LEC\\Desktop')
+    i+=1   
     
     
-    ###  Grafico funcion de correlación  total
-    plt.figure(6)
-#    plt.plot((dr*x), Gtotal_normalizada,'-.',label=f'D={d}')
-    plt.semilogx((dr*x), Gtotal_normalizada,'-.',label=f'D={d}')
+    ###  Grafico solo termino difussivo
+    plt.figure(i)
+#    plt.plot((dr*x), G_norm,'-.',label=f'D={d}',linewidth=1)
+    plt.semilogx((dr*x), G_diff_norm,'-.',label=f'D={d}')
     plt.legend()
-    plt.xlabel(r'pixel shift $\psi$ - $\mu m$',fontsize=14)
-    plt.ylabel(r'Gtot($\psi$)',fontsize=14)
-#    plt.title('V-line G total \n tl = {}$ms$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e3,dr,box_size),fontsize=18)
-    plt.title('V-line G total')
+    plt.xlabel(r'pixel shift $\xi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Gdiff($\xi$)',fontsize=14)
+#    plt.title('H-line G diff  \n tp = {}$\mu$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e6,dr,box_size),fontsize=18)
+    plt.title('V-line G diff')
     plt.show()
     plt.tight_layout() #hace que no me corte los márgenes
     
     if guardar_imagenes:
-        plt.savefig('C:\\Users\\LEC\\Desktop\\Poster TOPFOT 2019\\V_Line_Gtot_norm')
+        plt.savefig('C:\\Users\\LEC\\Desktop')
+    i+=1
 
+
+    ###  Grafico solo termino triplete
+    plt.figure(i)
+#    plt.plot((dr*x), G_norm,'-.',label=f'D={d}',linewidth=1)
+    plt.semilogx((dr*x), G_triplet_norm,'-.',label=f'D={d}')
+    plt.legend()
+    plt.xlabel(r'pixel shift $\xi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Gtriplet($\xi$)',fontsize=14)
+#    plt.title('H-line G diff  \n tp = {}$\mu$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e6,dr,box_size),fontsize=18)
+    plt.title('V-line G triplete')
+    plt.show()
+    plt.tight_layout() #hace que no me corte los márgenes
     
+    if guardar_imagenes:
+        plt.savefig('C:\\Users\\LEC\\Desktop')
+    i+=1
+    
+    
+    ###  Grafico solo termino binding
+    plt.figure(i)
+#    plt.plot((dr*x), G_norm,'-.',label=f'D={d}',linewidth=1)
+    plt.semilogx((dr*x), G_bind_norm,'-.',label=f'D={d}')
+    plt.legend()
+    plt.xlabel(r'pixel shift $\xi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Gbind($\xi$)',fontsize=14)
+#    plt.title('H-line G diff  \n tp = {}$\mu$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e6,dr,box_size),fontsize=18)
+    plt.title('V-line G binding')
+    plt.show()
+    plt.tight_layout() #hace que no me corte los márgenes
+    
+    if guardar_imagenes:
+        plt.savefig('C:\\Users\\LEC\\Desktop')
+    i+=1
+    
+    
+    ###  Grafico funcion de correlación  total
+    plt.figure(i)
+#    plt.plot((dr*x), Gtotal_normalizada,'-.',label=f'D={d}',linewidth=1)
+    plt.semilogx((dr*x), Gtotal_normalizada,'-.',label=f'D={d}')
+    plt.legend()
+    plt.xlabel(r'pixel shift $\xi$ - $\mu m$',fontsize=14)
+    plt.ylabel(r'Gtot($\xi$)',fontsize=14)
+    plt.title('H-line G total \n tp = {}$\mu$  - pix size = {} $\mu$- box size = {} pix'.format(tp*1e6,dr,box_size),fontsize=18)
+    plt.title('V-line  G total')
+    plt.show()
+    plt.tight_layout() #hace que no me corte los márgenes
+    
+    if guardar_imagenes:
+        plt.savefig('C:\\Users\\LEC\\Desktop')
+    i+=1    
 
 guardar_imagenes = False
     
