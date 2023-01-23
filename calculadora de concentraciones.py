76a89416454a9c22ebee09be22827fa1852e8cce
IgnacioSallaberry <42651786+IgnacioSallaberry@users.noreply.github.com>
2023-01-23 17:01:43 -0300

    Update calculadora de concentraciones.py

--- calculadora de concentraciones.py
+++ calculadora de concentraciones.py
@@ -16,13 +16,37 @@ roi = 128
 tp = 1e-6             #seg    
 tl = box_size * tp    #seg
 dr = 0.05             # delta r = pixel size =  micrometros
-w0 = 0.25              #radio de la PSF  = micrometros    
+w0 = 0.75              #radio de la PSF  = micrometros    
 wz = 1.5              #alto de la PSF desde el centro  = micrometros
 
+#%%
+#==============================================================================
+#                                  Para simulaciones en 2-Dimensiones
+#==============================================================================   
+vol_caja = (dr*box_size)**2 # OK
 
-vol_caja = (dr*box_size)**3 
+vol_PSF = (w0**2)*wz*1.04719 # OK esto es el valor reportado por el simFCS. No se de donde sale el factor 1.04719
 
-vol_PSF = (w0**2)*wz*((np.pi)**(1.5))
+vol_PSF_2Dimensiones = (w0**2)  # OK esto es en 2D
+
+partic_en_caja = 100
+
+partic_en_PSF = vol_PSF_2Dimensiones * partic_en_caja / vol_caja    #en femtolitros
+
+concentracion_en_PSF = partic_en_PSF/vol_PSF_2Dimensiones # cantidad de moleculas en PSF/ um^2
+#concentración en PSF está OK
+
+un_mol=6.023e23  ## moleculas en un mol
+
+concentracion_molar_en_PSF = concentracion_en_PSF/un_mol
+
+#%%
+#==============================================================================
+#                                  Para simulaciones en 3-Dimensiones
+#==============================================================================
+vol_caja = (dr*box_size)**3
+
+vol_PSF = ((w0*0.5)**2)*wz*((np.pi)**(1.5))
 
 partic_en_caja = 4000
 
